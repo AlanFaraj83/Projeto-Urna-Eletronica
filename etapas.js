bf24035d83eac2346a955423bc30f009b49253f5
ALAN FARAJ <alanfaraj@outlook.com>
2022-10-09 23:00:11 -0300

    Atualizando projeto

---
+++ etapas.js
@@ -0,0 +1,131 @@
+let etapas = [
+    {
+      titulo: 'PRESIDENTE',
+      numeros: 2,
+      candidatos: [
+        {
+          numero: '22',
+          nome: 'Jair Bolsonaro',
+          partido: 'PL',
+          fotos: [
+            {url: 'jair bolsonaro22.jpg', legenda: 'Presidente', small: false}
+          ]
+        },
+        {
+          numero: '13',
+          nome: 'Lula',
+          partido: 'PT',
+          fotos: [
+            {url: 'lula13.jpg', legenda: 'Presidente', small: false}
+          ]
+        },
+        {
+            numero: '15',
+            nome: 'Simone Tebet',
+            partido: 'MDB',
+            fotos: [
+              {url: 'Simone Tebet15.jpg', legenda: 'Presidente', small: false}
+            ]
+          },
+          {
+            numero: '12',
+            nome: 'Ciro Gomes',
+            partido: 'PDT',
+            fotos: [
+              {url: 'Ciro Gomes12.jpg', legenda: 'Presidente', small: false}
+            ]
+          },
+          {
+            numero: '30',
+            nome: 'Luiz Felipe d Avila',
+            partido: 'PL',
+            fotos: [
+              {url: 'Felipe D Avila30.jpg', legenda: 'Presidente', small: false}
+            ]
+          },
+          {
+            numero: '27',
+            nome: 'Eymael',
+            partido: 'PL',
+            fotos: [
+              {url: 'Eymael27.jpg', legenda: 'Presidente', small: false}
+            ]
+          },
+          {
+            numero: '44',
+            nome: 'Soraya Thronicke',
+            partido: 'Uniao',
+            fotos: [
+              {url: 'Soraya Thronicke44.jpg', legenda: 'Presidente', small: false}
+            ]
+          },
+          {
+            numero: '14',
+            nome: 'Padre Kelmon',
+            partido: 'PTB',
+            fotos: [
+              {url: 'Padre Kelmon14.jpg', legenda: 'Presidente', small: false}
+            ]
+          }
+          
+      ]
+    },
+    {
+      titulo: 'Governador',
+      numeros: 2,
+      candidatos: [
+        {
+          numero: '45',
+          nome: 'Rodrigo Garcia',
+          partido: 'PSDB',
+          vice: 'Ricardo Eletro',
+          fotos: [
+            {url: 'Rodrigo Garcia45.jpg', legenda: 'Governador', small: false},
+            {url: '', legenda: '', small: false}
+          ]
+        },
+        {
+          numero: '10',
+          nome: 'Haddad',
+          partido: 'PT',
+          vice: 'Pepe Moreno',
+          fotos: [
+            {url: 'Fernando Haddad10.jpg', legenda: 'Governador', small: false},
+            {url: '', legenda: '', small: false}
+          ]
+        },
+        {
+            numero: '16',
+            nome: 'Altino Junior',
+            partido: 'PSTU',
+            vice: 'Pepe Moreno',
+            fotos: [
+              {url: 'Altino Junior16.jpg', legenda: 'Governador', small: false},
+              {url: '', legenda: '', small: false}
+            ]
+         },
+         {
+            numero: '11',
+            nome: 'Tarcisio',
+            partido: 'REP',
+            vice: 'Pepe Moreno',
+            fotos: [
+              {url: 'Tarcisio de Freitas11.jpg', legenda: 'Governador', small: false},
+              {url: '', legenda: '', small: false}
+            ]
+         },
+         {
+            numero: '17',
+            nome: 'Elvis Cezar',
+            partido: 'PDT',
+            vice: 'Pepe Moreno',
+            fotos: [
+              {url: 'Elvis Cezar17.jpg', legenda: 'Governador', small: false},
+              {url: '', legenda: '', small: false}
+            ]
+         }
+        
+
+      ]
+    }
+  ]
